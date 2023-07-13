cbd78ccf3249f31322b43c001a0a6645c39c6e53
nashiusagi <ritotech0226@gmail.com>
2023-07-14 00:28:51 +0900

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header(){
+  return(
+    <div>
+      <header className="h-50 text-base p-12 flex justify-between max-w-full bg-sky-400">
+        <div>
+          <p>This is Header</p>
+        </div>
+        <div> 
+          <p>NashiUsa</p> 
+        </div>
+      </header>
+    </div>
+  )
+}
