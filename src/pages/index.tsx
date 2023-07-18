7799dbc4639646baa3c3124c746b9f9357e52aaf
nashiusagi <ritotech0226@gmail.com>
2023-07-18 22:10:14 +0900

    [update] fmt

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Header from '@/components/Header'
 export default function Home() {
   return (
     <div>
-      <div className="top-0 sticky z-50 left-0 right-0" >
+      <div className="top-0 sticky z-50 left-0 right-0">
         <Header />
       </div>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
